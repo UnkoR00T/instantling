b9356ebc7872b381c39e8c91f6b2d7c9068798bf
UnkoR00T <99119433+AlexYTGamePL@users.noreply.github.com>
2025-03-18 17:58:52 +0100

    Merge

--- instant.py
+++ instant.py
@@ -11,8 +11,8 @@ import time
 
 # OPTIONS
 url = 'https://instaling.pl/teacher.php?page=login'
-login_set = "xxx"
-password_set = "xxx"
+login_set = "2p2259788"
+password_set = "rxuxz"
 
 # Load JSON Data Safely
 try:
